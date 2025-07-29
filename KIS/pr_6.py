535dd383e78efed832ca92055b5afe37c2c08370
Fedor Shadaev <shadaevf@mail.ru>
2025-07-29 21:21:24 +0300

    Made some fixes with Ruff

--- KIS/pr_6.py
+++ KIS/pr_6.py
@@ -1,4 +1,4 @@
-from math import floor, ceil
+from math import ceil
 
 
 def main(x: set):
--- Working_script1.py
+++ Working_script1.py
@@ -6,9 +6,9 @@ import deal
 float) and isinstance(y2, float))
 @deal.post(lambda result: result >= 0)
 @deal.ensure(lambda x1, y1, x2, y2, result: result == ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5)
-@deal.reason(TypeError, lambda x1, x2, y1, y2: (isinstance(x1, (float)) == False and isinstance(y1, (float)) == False and isinstance(x2, (
-float)) == False and isinstance(y2, (float)) == False))
-@deal.raises(TypeError)
+@deal.reason(TypeError, lambda x1, x2, y1, y2: (not isinstance(x1, (float)) and not isinstance(y1, (float)) and not isinstance(x2, (
+float)) and not isinstance(y2, (float))))
+# @deal.raises(TypeError)
 @deal.has()
 
 
